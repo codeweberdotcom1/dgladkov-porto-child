9393e6a298705dad3746e9377a6157b2ad8067eb
Denis <web@dgladkov.ru>
2020-06-04 02:18:59 +0300

    Update functions.php
    Группипрвка подкатегорий

--- wp-content/themes/porto-child/functions.php
+++ wp-content/themes/porto-child/functions.php
@@ -15,3 +15,45 @@ function porto_child_css() {
 		wp_enqueue_style( 'styles-child-rtl' );
 	}
 }
+
+/**
+* Move WooCommerce subcategory list items into
+* their own <ul> separate from the product <ul>.
+*/
+
+add_action( 'init', 'move_subcat_lis' );
+
+function move_subcat_lis() {
+	// Remove the subcat <li>s from the old location.
+	remove_filter( 'woocommerce_product_loop_start', 'woocommerce_maybe_show_product_subcategories' );
+	add_action( 'woocommerce_before_shop_loop', 'msc_product_loop_start', 1 );
+	add_action( 'woocommerce_before_shop_loop', 'msc_maybe_show_product_subcategories', 2 );
+	add_action( 'woocommerce_before_shop_loop', 'msc_product_loop_end', 3 );
+}
+
+/**
+ * Conditonally start the product loop with a <ul> contaner if subcats exist.
+ */
+function msc_product_loop_start() {
+	$subcategories = woocommerce_maybe_show_product_subcategories();
+	if ( $subcategories ) {
+		woocommerce_product_loop_start();
+	}
+}
+
+/**
+ * Print the subcat <li>s in our new location.
+ */
+function msc_maybe_show_product_subcategories() {
+	echo woocommerce_maybe_show_product_subcategories();
+}
+
+/**
+ * Conditonally end the product loop with a </ul> if subcats exist.
+ */
+function msc_product_loop_end() {
+	$subcategories = woocommerce_maybe_show_product_subcategories();
+	if ( $subcategories ) {
+		woocommerce_product_loop_end();
+	}
+}
